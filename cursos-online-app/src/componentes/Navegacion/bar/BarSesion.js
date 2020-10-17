c1e1df9192db64e07623804aab8d2a58c2cb2d24
forozco <35968382+jsorozcof@users.noreply.github.com>
2020-10-16 22:15:23 -0500

    agragado toolbar izquierdo

--- cursos-online-app/src/componentes/Navegacion/bar/BarSesion.js
+++ cursos-online-app/src/componentes/Navegacion/bar/BarSesion.js
@@ -1,9 +1,35 @@
-import {Button,IconButton,Toolbar,Typography} from "@material-ui/core";
+/*import {
+  Button,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import React from "react";
 import theme from "../../../theme/theme";
 import FotoUsuarioTemp from "../../../logo.svg";
 import { useStateValue } from "../../../contexto/store";
+*/
+import React, { useState } from "react";
+import {
+  Toolbar,
+  IconButton,
+  Typography,
+  makeStyles,
+  Button,
+  Avatar,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+} from "@material-ui/core";
+import FotoUsuarioTemp from "../../../logo.svg";
+import { useStateValue } from "../../../contexto/store";
+import { MenuIzquierda } from "./menuIzquierda";
+import {MenuDerecha} from './menuDerecha';
+import { withRouter } from "react-router-dom";
+
 
 const useStyles = makeStyles((theme) => ({
   seccionDesktop: {
@@ -12,52 +38,109 @@ const useStyles = makeStyles((theme) => ({
       display: "flex",
     },
   },
-
   seccionMobile: {
     display: "flex",
     [theme.breakpoints.up("md")]: {
       display: "none",
     },
   },
-
   grow: {
     flexGrow: 1,
   },
-
   avatarSize: {
     width: 40,
     height: 40,
   },
+  list: {
+    width: 250,
+  },
+  listItemText: {
+    fontSize: "14px",
+    fontWeight: 600,
+    paddingLeft: "15px",
+    color: "#212121",
+  },
 }));
 
-const BarSesion = () => {
-    const classes = useStyles();
-    const [{ sesionUsuario }, dispatch] = useStateValue();
+const BarSesion = (props) => {
+  const classes = useStyles();
+  const [{ sesionUsuario }, dispatch] = useStateValue();
+
+  const [abrirMenuIzquierda, setAbrirMenuIzquierda] = useState(false);
+
+  const [abrirMenuDerecha, setAbrirMenuDerecha] = useState(false);
+
+  const cerrarMenuIzquierda = () => {
+    setAbrirMenuIzquierda(false);
+  };
+
+  const abrirMenuIzquierdaAction = () => {
+    setAbrirMenuIzquierda(true);
+  };
+
+  const cerrarMenuDerecha = () => {
+    setAbrirMenuDerecha(false);
+  };
+
+  const salirSesionApp = () => {
+    console.log('salir sesion');
+    localStorage.removeItem("token_seguridad");
+
+    dispatch({
+       type : "SALIR_SESION",
+       nuevoUsuario : null,
+       autenticado : false
+    })
+
+    props.history.push("/auth/login");
+  };
+
+  const abrirMenuDerechaAction = () => {
+    setAbrirMenuDerecha(true);
+  }
+
 
   return (
-    <Toolbar>
-      <IconButton color="inherit">
-        <i className="material-icons">menu</i>
-      </IconButton>
+    <React.Fragment>
+       <Drawer
+        open={abrirMenuIzquierda}
+        onClose={cerrarMenuIzquierda}
+        anchor="left"
+      >
+       <div className= {classes.list} onKeyDown={cerrarMenuIzquierda} onClick={cerrarMenuIzquierda}>
+        <List>
+          <ListItem button>
+            <i className="material-icons">account_box</i>
+            <ListItemText className={{primary: classes.listItemText}} primary="Perfil"/>
+          </ListItem>
+        </List>
+       </div>
+      
+      </Drawer>
 
-      <Typography variant="h6">Cursos Online</Typography>
-      <div className={classes.grow}></div>
-
-      <div className={classes.seccionDesktop}>
-        <Button color="inherit">Salir</Button>
-        <Button color="inherit">
-            {sesionUsuario ? sesionUsuario.usuario.nombreCompleto : "" } 
-        </Button>
-        
-      </div>
-
-      <div className={classes.seccionMobile}>
-        <IconButton color="inherit">
-            <i className="material-icons">more_vert</i>
+      <Toolbar>
+        <IconButton color="inherit" onClick={abrirMenuIzquierdaAction}>
+          <i className="material-icons">menu</i>
         </IconButton>
-      </div>
-    </Toolbar>
+
+        <Typography variant="h6">Cursos Online</Typography>
+        <div className={classes.grow}></div>
+
+        <div className={classes.seccionDesktop}>
+          <Button color="inherit">Salir</Button>
+          <Button color="inherit">
+            {sesionUsuario ? sesionUsuario.usuario.nombreCompleto : ""}
+          </Button>
+        </div>
+
+        <div className={classes.seccionMobile}>
+          <IconButton color="inherit">
+            <i className="material-icons">more_vert</i>
+          </IconButton>
+        </div>
+      </Toolbar>
+    </React.Fragment>
   );
 };
 
-export default  BarSesion;
+export default BarSesion;
---
+++ cursos-online-app/src/componentes/Navegacion/bar/menuDerecha.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { List, ListItem, Avatar, ListItemText } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import FotoUsuarioTemp from "../../../logo.svg";
+
+export const MenuDerecha = ( { 
+    classes,
+    usuario,
+    salirSesion
+}) => (
+    <div className={classes.list}>
+        <List>
+            <ListItem button component={Link}>
+                <Avatar src={ usuario.imagenPerfil || FotoUsuarioTemp }/>
+                <ListItemText classes={{primary : classes.listItemText}} primary={ usuario ? usuario.nombreCompleto : ""}/>
+            </ListItem>
+            <ListItem button onClick={salirSesion}>
+                <ListItemText classes={{primary : classes.listItemText}} primary = "Salir"/>
+            </ListItem>
+        </List>
+    </div>
+);
