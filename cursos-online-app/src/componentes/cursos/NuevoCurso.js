bec926232acd78ee0ca32ea738ef423efc88feaa
forozco <35968382+jsorozcof@users.noreply.github.com>
2020-10-22 21:49:38 -0500

    Cotrol MuiPickersUtilsProvider y variables de estado OK

--- cursos-online-app/src/componentes/cursos/NuevoCurso.js
+++ cursos-online-app/src/componentes/cursos/NuevoCurso.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import { Button, Container, Grid, TextField, Typography } from '@material-ui/core';
 import style from '../Tool/Style';
+import { KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import { useState } from 'react';
+
 
 const NuevoCurso = () => {
+    const [fechaSeleccionada, setFechaSeleccionada] = useState(new Date());
+
+    const [curso, setCurso] = useState({
+        titulo: '',
+        descripcion: '',
+        precio: 0.0,
+        promocion: 0.0
+    });
+
+    const ingresarValoresMemoria = e => {
+        const { name, value } = e.target;
+        setCurso( (anterior) => ({
+            ...anterior,
+            [name]: value
+        }));
+    }
+
     return (
       <Container component="main" maxWidth="md" justity="center">
         <div style={style.paper}>
@@ -14,40 +35,64 @@ const NuevoCurso = () => {
                 <Grid container spacing={2}>
                     <Grid item xs={12} md={12}>
                         <TextField
-                        name="Titulo"
+                        name="titulo"
                         variant="outlined"
                         fullWidth
                         label="Ingrese Titulo"
+                        value = {curso.titulo}
+                        onChange= {ingresarValoresMemoria}
                         />
                     </Grid>
 
                     <Grid item xs={12} md={12}>
                         <TextField
-                        name="Descripcion"
+                        name="descripcion"
                         variant="outlined"
                         fullWidth
-                        label="Ingrese Descripcion"
+                        label="Ingrese descripcion"
+                        value = {curso.descripcion}
+                        onChange= {ingresarValoresMemoria}
                         />
                     </Grid>
 
                     <Grid item xs={12} md={6}>
                         <TextField
-                        name="Precio"
+                        name="precio"
                         variant="outlined"
                         fullWidth
                         label="Ingrese Precio Normal"
+                        value = {curso.precio}
+                        onChange= {ingresarValoresMemoria}
                         />
                     </Grid>
 
                     <Grid item xs={12} md={6}>
                         <TextField
-                        name="Promocion"
+                        name="promocion"
                         variant="outlined"
                         fullWidth
                         label="Ingrese Precio Promocion"
+                        value = {curso.promocion}
+                        onChange= {ingresarValoresMemoria}
                         />
                     </Grid>
 
+                    <Grid item xs={12} md={6}>
+                       <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                        <KeyboardDatePicker 
+                        value={fechaSeleccionada}
+                        onChange={setFechaSeleccionada}
+                        margin="normal"
+                        id="fecha-publicacion-id"
+                        label="Seleccione fecha de publicacion"
+                        format="dd/MM/yyyy"
+                        fullWidth
+                        KeyboardButtonProps = {{
+                            "aria-label": "change date"
+                        }}
+                        />
+                       </MuiPickersUtilsProvider>
+                    </Grid>
                 </Grid>
             
                 <Grid container justify="center">
